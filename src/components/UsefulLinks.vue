9531ef73b5599835acc88a4b2d63afa43dafc27f
Alessandro Oviszach <78302740+AlessandroOviszach@users.noreply.github.com>
2021-12-14 16:55:13 +0100

    setup data link section

---
+++ src/components/UsefulLinks.vue
@@ -0,0 +1,41 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    name : 'UsefulLinks',
+    data() {
+        return {
+            ContactList : [
+                {
+                    icon : '',
+                    text : '',
+                }
+            ],
+            SupportList : [
+                {
+                    icon : '',
+                    text : '',
+                }
+            ],
+            ExploreList : [
+                {
+                    icon : '',
+                    text : '',
+                }
+            ],
+            VistList : [
+                {
+                    icon : '',
+                    text : '',
+                }
+            ],
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
